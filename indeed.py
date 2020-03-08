87f177f9cca1d91d0ff3732a2792675a0525048b
Sung-Jun Jin <jinsungjun94@gmail.com>
2020-03-08 15:20:03 +0900

    2.5 Requesting Each Page까지

--- indeed.py
+++ indeed.py
@@ -1,39 +1,43 @@
 # indeed URL
-# https://kr.indeed.com/jobs?q=python&limit=50&radius=25
+# "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50"
 
 import requests
 from bs4 import BeautifulSoup
 
 # 파이썬에서 요청을 만드는 기능을 모아두는 모듈
 
-INDEED_URL = "https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit=50"
-LIMIT = 50
+LIMIT = 50 # 한 페이지에 보여줄 구인광고 리스트의 수 
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?q=python&limit={LIMIT}"
+
 
 def extract_indeed_pages() :
 
-    result = requests.get(INDEED_URL)
+    result = requests.get(URL)
 
     soup = BeautifulSoup(result.text,'html.parser')
 
-    pagination = soup.find("div",{"class" : "pagination"})
+    pagination = soup.find("div",{"class" : "pagination"}) # <div class="pagination"> 엘리먼트 추출
 
-    links = pagination.find_all("a")
+    links = pagination.find_all("a") # 추출된 엘리먼트 중에서 <a> 태그 엘리먼트를 추출 
 
 
     pages = []
     for link in links[:-1] :  
-        pages.append(int(link.find("span").string))
+        pages.append(int(link.find("span").string)) # 각 페이지의 번호를 pages list 에 옮겨담는다 
 
-    max_page = pages[-1]
+    # 가장 마지막 페이지의 번호를 max_page에 담는다 
+    max_page = pages[-1] # python에서 배열의 마지막 index는 -1이다. 
 
     return max_page
 
 
 def extract_indeed_jobs(last_page) :
-
+    # indeed 사이트의 각 페이지 번호마다 URL을 request하기 위해 1부터 마지막 페이지인 10까지의 URL을 가져온다.
     for page in range(0,last_page) :
-
-        print(f"{INDEED_URL}&start={page * 50}") 
+      
+        result = requests.get(f"{URL}&start={page * LIMIT}") # 1~10 페이지의 URL을 request
+        
+        print(f"{page} status code = {result.status_code}") # 각 페이지의 status code를 출력
 
 
 extract_indeed_jobs(extract_indeed_pages())
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 # indeed URL
 # https://kr.indeed.com/jobs?q=python&limit=50&radius=25
 
-from indeed import extract_indeed_pages # indeed.py 에서 extract_indeed_pages() 함수를 import
+from indeed import extract_indeed_pages, extract_indeed_jobs # indeed.py 에서 extract_indeed_pages() 함수를 import
 
 
-max_indeed_pages = extract_indeed_pages()
+last_indeed_page = extract_indeed_pages()
 
-for n in range(max_indeed_pages) :
-
-    print(f"start={n * 50}") # python f print문 연습하기
+extract_indeed_jobs(last_indeed_page)
         
 
      
