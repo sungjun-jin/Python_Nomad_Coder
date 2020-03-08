4fa60c3ea7e5c2c3ab9e148a7a4536d4df349889
Sung-Jun Jin <jinsungjun94@gmail.com>
2020-03-08 18:06:52 +0900

    extract datas from indeed

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from indeed import extract_indeed_pages, extract_indeed_jobs # indeed.py 에서 
 
 last_indeed_page = extract_indeed_pages()
 
-extract_indeed_jobs(last_indeed_page)
-        
+indeed_jobs = extract_indeed_jobs(last_indeed_page) 
+
 
      
+
+
 
 
